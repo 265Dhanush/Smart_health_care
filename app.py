21d212e86b09bb538d26c274514992471ca72baf
265Dhanush <159248202+265Dhanush@users.noreply.github.com>
2024-07-06 15:35:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+import mysql.connector
+from database import mydb
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+
+@app.route('/pres')
+def prescription():
+  return render_template('pres.html')
+
+
+@app.route('/bmi')
+def bmi():
+  return render_template('bmi.html')
+
+
+@app.route('/bpm')
+def bpm():
+  return render_template('bpm.html')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
+@app.route('/')
+def hello_world():
+  patients = load_db()
+  return render_template('home.html', patients=patients)
+
+
+@app.route("/submit", methods=["POST"])
+def submit_form():
+  name = request.form["name"]
+  age = request.form["age"]
+  date = request.form["date"]
+  time = request.form["time"]
+  room_temperature = request.form["room_temperature"]
+  room_humidity = request.form["room_humidity"]
+  pulse_rate = request.form["pulse_rate"]
+  body_temperature = request.form["body_temperature"]
+
+  cursor = mydb.cursor()
+  sql = "INSERT INTO patients (p_date,p_time,p_name,age,pulse_rate,body_temperature,room_temperature,room_humidity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+  val = (date, time, name, age, pulse_rate, body_temperature, room_temperature,
+         room_humidity)
+  cursor.execute(sql, val)
+  mydb.commit()
+
+  return "Form submitted successfully"
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="database-1.cjcyeqckaoos.eu-north-1.rds.amazonaws.com",
+    user="admin",
+    password="abhilash",
+    port=3306,
+    database="patient_database")
+
+
+
+  
+
+
+
